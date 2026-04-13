f9295879539ee30858931643382e5ca0c86dd4d9
Pavel <pavelpopov240306@gmail.com>
2026-04-13 23:47:33 +0300

    ## Задача 2

--- rk_1/main.cpp
+++ rk_1/main.cpp
@@ -11,4 +11,17 @@ int main() {
     std::cout << temp2;
     
     return 0;
+}#include "fraction.h"
+#include <iostream>
+
+int main() {
+    Fraction fr1(10, 20);
+    Fraction fr2(5, 7);
+    
+    std::cout << sum(fr1, fr2) << std::endl;
+    std::cout << sub(fr1, fr2) << std::endl;
+    std::cout << div(fr1, fr2) << std::endl;
+    std::cout << mul(fr1, fr2) << std::endl;
+    
+    return 0;
 }
